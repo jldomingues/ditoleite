58a8261a1ad7b52313b63e4ebe1772736e4f83bd
José Lucas Domingues <97459319+jldomingues@users.noreply.github.com>
2022-06-02 22:29:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function galo(){
+var audio1 = new Audio();
+    audio1.src = "galo.mp3";
+    audio1.play();
+}
+
+function musiquinha(){
+var audio1 = new Audio();
+    audio1.src = "musiquinha.mp3";
+    audio1.play();
+}
+
+function palminha(){
+var audio1 = new Audio();
+    audio1.src = "palminha.mp3";
+    audio1.play();
+}
+
+
+    
